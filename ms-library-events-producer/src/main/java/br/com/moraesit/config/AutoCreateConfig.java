92aa5dbb8625010d337ccfee8133e360a36a0e91
joao.santos2 <joao.santos@ti9.com.br>
2020-12-19 20:42:02 -0300

    auto create kafka topic with KafkaAdmin

---
+++ ms-library-events-producer/src/main/java/br/com/moraesit/config/AutoCreateConfig.java
@@ -0,0 +1,22 @@
+package br.com.moraesit.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+
+// automatic topic creation is not recommended in production environment.
+@Configuration
+@Profile("local")
+public class AutoCreateConfig {
+
+    @Bean
+    public NewTopic libraryEvents() {
+        return TopicBuilder
+                .name("library-events")
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+}
