67e339e2f077b805c0c048e5397e74b1dd50bb98
joao.santos2 <joao.santos@ti9.com.br>
2020-12-21 14:38:30 -0300

    Set up the library events consumer project

---
+++ ms-library-events-consumer/src/main/java/br/com/moraesit/MsLibraryEventsConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.moraesit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLibraryEventsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLibraryEventsConsumerApplication.class, args);
+	}
+
+}
