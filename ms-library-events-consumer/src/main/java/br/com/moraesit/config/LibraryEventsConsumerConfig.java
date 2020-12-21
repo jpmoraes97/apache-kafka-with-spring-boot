690979ccd070a5b6c13932bc368952a32faa3a7e
joao.santos2 <joao.santos@ti9.com.br>
2020-12-21 15:02:16 -0300

    Configure Kafka consumer using sprigboot profiles

---
+++ ms-library-events-consumer/src/main/java/br/com/moraesit/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package br.com.moraesit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
